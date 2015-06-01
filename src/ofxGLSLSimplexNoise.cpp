9b08fdbf270a634f5c5df0751190fd72475083b6
Atsushi Tadokoro <tadokoro@gmail.com>
2015-06-01 12:24:52 +0900

    update

--- src/ofxGLSLSimplexNoise.cpp
+++ src/ofxGLSLSimplexNoise.cpp
@@ -1,1 +1,21 @@
 #include "ofxGLSLSimplexNoise.h"
+
+ofxGLSLSimplexNoise::ofxGLSLSimplexNoise(){   
+    shader.load("../../../shaders/simplexnoise.vert", "../../../shaders/simplexnoise.frag");
+    freq = 1.0;
+    mul = 1.0;
+    add = 0.0;
+    shift.set(0.0, 0.0);
+}
+
+void ofxGLSLSimplexNoise::draw(){
+    shader.begin();
+    shader.setUniform1f("time", ofGetElapsedTimef());
+    shader.setUniform1f("freq", freq);
+    shader.setUniform1f("mul", mul);
+    shader.setUniform1f("add", add);
+    shader.setUniform2f("shift", shift.x, shift.y);
+    ofScale(ofGetWidth(), ofGetWidth());
+    ofRect(0, 0, 1.0, 1.0);
+    shader.end();
+}
--- src/ofxGLSLSimplexNoise.h
+++ src/ofxGLSLSimplexNoise.h
@@ -1,7 +1,14 @@
 #pragma once
+#include "ofMain.h"
 
 class ofxGLSLSimplexNoise {
 public:
     ofxGLSLSimplexNoise();
     void draw();
+    
+    ofShader shader;
+    float freq;
+    float mul;
+    float add;
+    ofVec2f shift;
 };
